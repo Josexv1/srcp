7e56cec029ca18762616b6b361117d9c0caedb8c
Jose <Jozhee@live.com>
2015-05-04 21:48:42 -0430

    Mejorado mensajes de error

--- config.php
+++ config.php
@@ -8,8 +8,14 @@
 
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
     try { $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); }
-    catch(PDOException $ex){ die("Fallamos al conectarnos a la base de datos, el error es el siguiente: " . $ex->getMessage());}
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    catch(PDOException $ex){ 
+        echo "<div class='panel-body'>
+                    <div class='alert alert-warning alert-dismissable'>
+                          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                          Tenemos problemas al ejecutar la consulta. El error es el siguiente: 
+                    </div>
+                  </div>" .$ex->getMessage();}
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     header('Content-Type: text/html; charset=utf-8');
 
